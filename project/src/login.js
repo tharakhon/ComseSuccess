dfb15f137c1351e768160cd09096fbd636221316
tharakhon <earth0981234@gmail.com>
2023-10-18 22:57:39 +0700

    Success

--- project/src/login.js
+++ project/src/login.js
@@ -13,6 +13,7 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
 
 
 function Login() {
@@ -135,8 +136,23 @@ function Login() {
   }
   return (
     <div>
-      <Container component="main" maxWidth="xs">
+      <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <Box
           sx={{
             marginTop: 8,
@@ -145,6 +161,7 @@ function Login() {
             alignItems: 'center',
           }}
         >
+
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
@@ -200,7 +217,8 @@ function Login() {
             </Grid>
           </Box>
         </Box>
-      </Container>
+        </Grid>
+        </Grid>
   </div>
   )
 }
--- project/src/profile.js
+++ project/src/profile.js
@@ -130,7 +130,7 @@ export default function Login() {
         <Toolbar>
           <CameraIcon sx={{ mr: 2 }} />
           <Typography variant="h6" color="inherit" noWrap>
-            Album layout
+            Comsecurity
           </Typography>
         </Toolbar>
       </AppBar>
@@ -151,12 +151,10 @@ export default function Login() {
               color="text.primary"
               gutterBottom
             >
-              Album layout
+              Welcome!
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
+              Your have succesfully login.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -187,17 +185,13 @@ export default function Login() {
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      Photo
                     </Typography>
                     <Typography>
                       This is a media card. You can use this section to describe the
                       content.
                     </Typography>
                   </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
                 </Card>
               </Grid>
             ))}

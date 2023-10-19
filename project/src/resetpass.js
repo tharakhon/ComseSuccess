1e3dad9101a2dd9e5b5a8d8d6bb4ada47a16552e
tharakhon <earth0981234@gmail.com>
2023-10-19 20:54:04 +0700

    Success

--- project/src/resetpass.js
+++ project/src/resetpass.js
@@ -69,7 +69,7 @@ function Resetpass() {
         return;
       }
           // ตรวจสอบว่ารหัสผ่านมีตัวอักษรพิมพ์ใหญ่และตัวอักษรพิมพ์เล็ก
-          if (!/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/[!@#$%^&*]/.test(password) || !/[0-9]/.test(password)) {
+          if (!/[a-z]/.test(newpassword) || !/[A-Z]/.test(newpassword) || !/[!@#$%^&*]/.test(newpassword) || !/[0-9]/.test(newpassword)) {
             MySwal.fire({
               html: <i>Password must contain lowercase letters, uppercase letters, and at least one special character (!@#$%^&*).</i>,
               icon: 'error',
